ed7b7bf69e1676072763c10db6e549a978d996ea
Lisa <jan_friemal@gmx.de>
2020-05-17 17:28:31 +0200

    Lisa Homework

--- assignment_2.py
+++ assignment_2.py
@@ -3,8 +3,23 @@ def translate_numeral(number):
 		roman_numeral = number
 
 		# ======enter code below======
-		arabic_number = None
-		
+		arabic_number = 0
+		pups = {"I": 1, "X": 10, "C": 100, "M": 1000, "V": 5, "L": 50, "D": 500}
+		translated_number = []
+		for letter in roman_numeral:
+			translated_number.append(pups[letter])
+
+		print(translated_number)
+
+		last_number = 0
+		for current_number in translated_number:
+			if current_number <= last_number:
+				arabic_number = arabic_number + last_number
+			else:
+				arabic_number = arabic_number - last_number
+			last_number = current_number
+		arabic_number = arabic_number + last_number
+
 		# ======enter code above======
 		print('{0} translated to {1}!'.format(roman_numeral, arabic_number))  # print the result
 		return arabic_number 
